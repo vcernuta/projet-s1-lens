cc359919ae134c74fe06acaa89a2f795abf8825b
Valentin Cernuta <val.cernuta@gmail.com>
2018-12-09 16:34:17 +0100

    ajout de la navigation pour la partie anglaise et mise à jour des liens

---
+++ js/enNavigationDesktop.js
@@ -0,0 +1,42 @@
+let data = `
+<ul class="self-menu">
+<li class="self-link">
+    <a href="../index.html">Home</a>
+    <ul class="self-menu--submenu">
+        <li class="self-menu--submenu--link">
+            <a href="#">Teacher access</a>
+        </li>
+    </ul>
+</li>
+<li class="self-link">
+    <a href="">Training</a>
+    <ul class="self-menu--submenu">
+        <li class="self-menu--submenu--link"><a href="../formation/iut.html">The IUT</a></li>
+        <li class="self-menu--submenu--link"><a href="../formation/departement-info.html">Computing
+                Dpts</a></li>
+        <li class="self-menu--submenu--link"><a href="../formation/programme.html">Program</a></li>
+        <li class="self-menu--submenu--link"><a href="../formation/carte-iut.html">Computer
+                Dpts map</a></li>
+    </ul>
+</li>
+<li class="self-link">
+    <a href="">Links</a>
+    <ul class="self-menu--submenu">
+        <li class="self-menu--submenu--link"><a href="../liens/temoignages-en.html">Testimonials</a></li>
+        <li class="self-menu--submenu--link"><a href="../liens/presse-en.html">Newspaper</a></li>
+    </ul>
+</li>
+<li class="self-link">
+    <a href="../partenaires-en.html">Partners</a>
+</li>
+<li class="self-link">
+    <a href="../entreprises-en.html">Companies</a>
+</li>
+
+<li class="self-link right">
+    <a href="../../fr/index.html">Switch to French</a>
+</li>
+</ul>
+`
+
+document.write(data)
---
+++ js/enNavigationMobile.js
@@ -0,0 +1,18 @@
+let dataMobile = `
+<span class="mdl-layout-title">IUT Informatique</span>
+            <nav class="mdl-navigation">
+                <a class="mdl-navigation__link" href="index.html">Home</a>
+                <a class="mdl-navigation__link" href="#">Teacher access</a>
+                <a class="mdl-navigation__link" href="./formation/iut.html">The IUT</a>
+                <a href="./formation/departement-info.html" class="mdl-navigation__link">Computing Dpts</a>
+                <a href="./formation/programme.html" class="mdl-navigation__link">Program</a>
+                <a href="./formation/carte-iut.html" class="mdl-navigation__link">Computing Dpts map</a>
+                <a class="mdl-navigation__link" href="./liens/temoignages.html">Testimonials</a>
+                <a href="./liens/presse.html" class="mdl-navigation__link">Newspaper</a>
+                <a class="mdl-navigation__link" href="./partenaires.html">Partners</a>
+                <a class="mdl-navigation__link" href="./entreprises.html">Companies</a>
+                <a href="./en/index.html" class="mdl-navigation__link">Switch to French</a>
+            </nav>
+`
+
+document.write(dataMobile)
